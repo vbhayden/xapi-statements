61ebf9c2c6ad5ffb6d28922650b9ccf79d007976
Ryan Smith <0ryansmith1994@gmail.com>
2017-04-12 17:41:08 +0100

    Reduces code duplication.

--- src/tests/storingStatements/conflicts.test.ts
+++ src/tests/storingStatements/conflicts.test.ts
@@ -5,6 +5,11 @@ import setup from '../utils/setup';
 import createStatement from '../utils/createStatement';
 
 const TEST_ID = '1c86d8e9-f325-404f-b3d9-24c451035582';
+const TEST_STATEMENT = createStatement({ id: TEST_ID });
+const TEST_CONFLICT = createStatement({
+  id: TEST_ID,
+  actor: {mbox: 'mailto:test2@example.com'},
+});
 
 describe('store statement conflicts', () => {
   const service = setup();
@@ -17,19 +22,16 @@ describe('store statement conflicts', () => {
   };
 
   it('should store statements when they use an existing id without conflicts in 2 batches', async () => {
-    await storeStatements([createStatement({ id: TEST_ID })]);
-    const ids: string[] = await storeStatements([createStatement({ id: TEST_ID })]);
+    await storeStatements([TEST_STATEMENT]);
+    const ids: string[] = await storeStatements([TEST_STATEMENT]);
     assert.equal(isArray(ids), true);
     assert.deepEqual(ids, [TEST_ID]);
   });
 
   it('should not store statements when they use an existing id with conflicts in 2 batches', async () => {
     try {
-      await storeStatements([createStatement({ id: TEST_ID })]);
-      await storeStatements([createStatement({
-        id: TEST_ID,
-        actor: {mbox: 'mailto:test2@example.com'},
-      })]);
+      await storeStatements([TEST_STATEMENT]);
+      await storeStatements([TEST_CONFLICT]);
       assert(false);
     } catch (err) {
       assert.equal(err.constructor, Conflict);
@@ -37,23 +39,14 @@ describe('store statement conflicts', () => {
   });
 
   it('should store statements when they use an existing id without conflicts in 1 batch', async () => {
-    const ids: string[] = await storeStatements([
-      createStatement({ id: TEST_ID }),
-      createStatement({ id: TEST_ID }),
-    ]);
+    const ids: string[] = await storeStatements([TEST_STATEMENT, TEST_STATEMENT]);
     assert.equal(isArray(ids), true);
     assert.deepEqual(ids, [TEST_ID, TEST_ID]);
   });
 
   it('should not store statements when they use an existing id with conflicts in 1 batch', async () => {
     try {
-      await storeStatements([
-        createStatement({ id: TEST_ID }),
-        createStatement({
-          id: TEST_ID,
-          actor: {mbox: 'mailto:test2@example.com'},
-        }),
-      ]);
+      await storeStatements([TEST_STATEMENT, TEST_CONFLICT]);
       assert(false);
     } catch (err) {
       assert.equal(err.constructor, Conflict);
