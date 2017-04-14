83edf0b4612fcac0ab88fad3d005fcc81a72b56b
Ryan Smith <0ryansmith1994@gmail.com>
2017-04-14 01:33:37 +0100

    Adds test for registration filtering.

--- src/tests/getStatements/filters.test.ts
+++ src/tests/getStatements/filters.test.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert';
 import { isArray } from 'lodash';
+import GetStatementsOptions from '../../service/options/GetStatementsOptions';
 import setup from '../utils/setup';
 import createStatement from '../utils/createStatement';
 
@@ -13,18 +14,44 @@ describe('get statement', () => {
     });
   };
 
+  const getStatements = (opts: GetStatementsOptions) => {
+    return service.getExactStatements(opts);
+  };
+
+  const filterStatements = async (statements: any[], opts: GetStatementsOptions) => {
+    await storeStatements(statements);
+    const filteredStatements = await getStatements(opts);
+    assert(isArray(filteredStatements));
+    assert.equal(filteredStatements.length, 1);
+    return filteredStatements;
+  };
+
   it('should return statements when they match the verb', async () => {
     const verbId1 = 'http://www.example.com/verb1';
     const verbId2 = 'http://www.example.com/verb2';
-    await storeStatements([
+    const statements = await filterStatements([
       createStatement({ verb: { id: verbId1 }}),
       createStatement({ verb: { id: verbId2 }}),
-    ]);
-    const statements = await service.getExactStatements({
+    ], {
       verb: verbId1,
     });
-    assert(isArray(statements));
-    assert.equal(statements.length, 1);
     assert.equal(statements[0].verb.id, verbId1);
+  });
+
+  it('should return statements when they match the verb', async () => {
+    const reg1 = '1c86d8e9-f325-404f-b3d9-24c451035583';
+    const reg2 = '1c86d8e9-f325-404f-b3d9-24c451035584';
+    const statements = await filterStatements([
+      createStatement({ context: { registration: reg1 }}),
+      createStatement({ context: { registration: reg2 }}),
+    ], {
+      registration: reg1,
+    });
+    const context = statements[0].context;
+    if (context !== undefined) {
+      assert.equal(context.registration, reg1);
+    } else {
+      assert(false, 'Expected context to be defined');
+    }
   });
 });
