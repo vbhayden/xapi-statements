5113d233f6c89a82b7525404d5cadc3ca6c66085
Ryan Smith <0ryansmith1994@gmail.com>
2017-04-19 16:21:56 +0100

    Adds tests for group members formatted by ids.

--- src/tests/getIdsStatements/utils/groupTest.ts
+++ src/tests/getIdsStatements/utils/groupTest.ts
@@ -9,4 +9,29 @@ export default (createActorStatement: (actor: any) => any) => {
       };
     })(createActorStatement);
   });
+
+  describe('identified group members', () => {
+    agentFormatTest((ifi: any): any => {
+      return {
+        mbox: 'mailto:test@example.com',
+        objectType: 'Group',
+        member: [{
+          objectType: 'Agent',
+          ...ifi,
+        }]
+      };
+    })(createActorStatement);
+  });
+
+  describe('anonymous group members', () => {
+    agentFormatTest((ifi: any): any => {
+      return {
+        objectType: 'Group',
+        member: [{
+          objectType: 'Agent',
+          ...ifi,
+        }]
+      };
+    })(createActorStatement);
+  });
 };
