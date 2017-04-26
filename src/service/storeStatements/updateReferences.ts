0d7ac91de6ac66649e7443e36863920194afa74e
Ryan Smith <0ryansmith1994@gmail.com>
2017-04-26 16:51:40 +0100

    Finishes implementation of references (more testing needed).

--- src/service/storeStatements/updateReferences.ts
+++ src/service/storeStatements/updateReferences.ts
@@ -2,19 +2,20 @@ import { includes, union } from 'lodash';
 import NoModel from '../../errors/NoModel';
 import StatementModel from '../../models/StatementModel';
 import Config from '../Config';
+import logger from '../../logger';
 
-export default async (
-  config: Config,
-  models: StatementModel[]
-): Promise<void> => {
+export default async (config: Config, models: StatementModel[]): Promise<void> => {
   if (!config.enableReferencing) return;
+
   const traverseDown = async (modelId: string, visitedIds: string[]): Promise<string[]> => {
+    logger.debug('traverseDown', modelId, visitedIds);
     try {
+      const newVisitedIds = [modelId, ...visitedIds];
       const downRefId = await config.repo.getDownRefId({ id: modelId });
       return (
-        includes(visitedIds, downRefId) ?
-        traverseUp([], visitedIds, downRefId) :
-        traverseDown(downRefId, visitedIds)
+        includes(newVisitedIds, downRefId) ?
+        traverseUp([], newVisitedIds, downRefId) :
+        traverseDown(downRefId, newVisitedIds)
       );
     } catch (err) {
       if (err.constructor === NoModel) {
@@ -29,8 +30,9 @@ export default async (
     refIds: string[],
     modelId: string
   ): Promise<string[]> => {
+    logger.debug('traverseUp', visitedIds, refIds, modelId);
     if (includes(visitedIds, modelId)) return [];
-    await config.repo.setRefs({ id: modelId, refIds });
+    if (refIds.length > 0) await config.repo.setRefs({ id: modelId, refIds });
 
     const newVisitedIds = [modelId, ...visitedIds];
     const newRefIds = [modelId, ...refIds];
@@ -43,6 +45,7 @@ export default async (
     refIds: string[],
     upRefIds: string[]
   ): Promise<string[]> => {
+    logger.debug('traverseUpRefs', visitedIds, refIds, upRefIds);
     const traversedIds: string[][] = await Promise.all(upRefIds.map((upRefId) => {
       return traverseUp(visitedIds, refIds, upRefId);
     }));
@@ -51,6 +54,9 @@ export default async (
 
   await models.reduce(async (results, model): Promise<string[]> => {
     const visitedIds = await results;
-    return traverseDown(model.statement.id, visitedIds);
+    const modelId = model.statement.id;
+    if (includes(visitedIds, modelId)) return visitedIds;
+    logger.debug('Updating references', modelId);
+    return traverseDown(modelId, []);
   }, Promise.resolve([]));
 };
--- src/tests/storingStatements/references.test.ts
+++ src/tests/storingStatements/references.test.ts
@@ -8,7 +8,7 @@ const TEST_ID_A = '1c86d8e9-f325-404f-b3d9-24c45103558A';
 const TEST_ID_B = '1c86d8e9-f325-404f-b3d9-24c45103558B';
 const TEST_ID_C = '1c86d8e9-f325-404f-b3d9-24c45103558C';
 const TEST_ID_D = '1c86d8e9-f325-404f-b3d9-24c45103558D';
-const TEST_ID_E = '1c86d8e9-f325-404f-b3d9-24c45103558E';
+// const TEST_ID_E = '1c86d8e9-f325-404f-b3d9-24c45103558E';
 
 describe('store statements references', () => {
   const service = setup();
@@ -53,19 +53,51 @@ describe('store statements references', () => {
 
   it('should return no statements when targeted statement is not stored', async () => {
     await storeStatements([
-      createTargetingStatement(TEST_ID_A, TEST_ID_E),
+      createTargetingStatement(TEST_ID_A, TEST_ID_B),
     ]);
-    await assertTargetingStatement(TEST_ID_E, []);
+    await assertTargetingStatement(TEST_ID_A, [TEST_ID_A]);
+    await assertTargetingStatement(TEST_ID_B, []);
   });
 
-  it('should return no statements when targeted statement is not stored', async () => {
+  it('should return both statements when they reference each other in one batch', async () => {
     await storeStatements([
-      createTargetingStatement(TEST_ID_A, TEST_ID_E),
-      createTargetingStatement(TEST_ID_C, TEST_ID_A),
-      createTargetingStatement(TEST_ID_D, TEST_ID_B),
+      createTargetingStatement(TEST_ID_A, TEST_ID_B),
+      createTargetingStatement(TEST_ID_B, TEST_ID_A),
     ]);
-    await assertTargetingStatement(TEST_ID_E, []);
-    await assertTargetingStatement(TEST_ID_A, [TEST_ID_A, TEST_ID_C]);
-    await assertTargetingStatement(TEST_ID_B, []);
+    await assertTargetingStatement(TEST_ID_A, [TEST_ID_A, TEST_ID_B]);
+    await assertTargetingStatement(TEST_ID_B, [TEST_ID_A, TEST_ID_B]);
+  });
+
+  it('should return both statements when they reference each other in two batches', async () => {
+    await storeStatements([
+      createTargetingStatement(TEST_ID_A, TEST_ID_B),
+    ]);
+    await storeStatements([
+      createTargetingStatement(TEST_ID_B, TEST_ID_A),
+    ]);
+    await assertTargetingStatement(TEST_ID_A, [TEST_ID_A, TEST_ID_B]);
+    await assertTargetingStatement(TEST_ID_B, [TEST_ID_A, TEST_ID_B]);
+  });
+
+  it('should return three statements when two target one', async () => {
+    await storeStatements([
+      createTargetingStatement(TEST_ID_A, TEST_ID_C),
+      createTargetingStatement(TEST_ID_B, TEST_ID_C),
+      createTargetingStatement(TEST_ID_C, TEST_ID_D),
+    ]);
+    await assertTargetingStatement(TEST_ID_C, [TEST_ID_A, TEST_ID_B, TEST_ID_C]);
+    await assertTargetingStatement(TEST_ID_B, [TEST_ID_B]);
+    await assertTargetingStatement(TEST_ID_A, [TEST_ID_A]);
+  });
+
+  it('should return all statements when the references cycle', async () => {
+    await storeStatements([
+      createTargetingStatement(TEST_ID_A, TEST_ID_B),
+      createTargetingStatement(TEST_ID_B, TEST_ID_C),
+      createTargetingStatement(TEST_ID_C, TEST_ID_A),
+    ]);
+    await assertTargetingStatement(TEST_ID_A, [TEST_ID_A, TEST_ID_B, TEST_ID_C]);
+    await assertTargetingStatement(TEST_ID_B, [TEST_ID_A, TEST_ID_B, TEST_ID_C]);
+    await assertTargetingStatement(TEST_ID_C, [TEST_ID_A, TEST_ID_B, TEST_ID_C]);
   });
 });
