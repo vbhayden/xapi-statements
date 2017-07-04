1fc283e37c4d4eb9384b6f00e8fcc8f470cfe377
Ryan Smith <0ryansmith1994@gmail.com>
2017-07-04 15:56:06 +0100

    Uses jscommons for logger.

--- src/logger.ts
+++ src/logger.ts
@@ -1,30 +1,18 @@
-import * as moment from 'moment';
-import * as winston from 'winston';
-import * as CloudWatchTransport from 'winston-aws-cloudwatch';
+import winston from 'jscommons/dist/winston';
 import config from './config';
 
-const getTime = () => moment().format('YYYY-MM-DD HH:mm:ss:SSS');
-
-winston.cli();
-export default new winston.Logger({
-  exitOnError: false,
-  transports: [
-    new (winston.transports.Console)({
-      handleExceptions: true,
-      colorize: true,
-      timestamp: getTime,
-      prettyPrint: true,
-      humanReadableUnhandledException: true,
-      level: config.winston.level,
-      stderrLevels: ['error']
-    }),
-    ...(
-      config.winston.enableAws === false ? [] : [
-        new CloudWatchTransport({
-          level: config.winston.level,
-          ...config.winston.aws,
-        }),
-      ]
-    )
-  ]
+export default winston({
+  console: {
+    level: config.winston.level,
+  },
+  cloudWatch: {
+    level: config.winston.level,
+    enabled: config.winston.enableAws,
+    logGroupName: config.winston.aws.logGroupName,
+    awsConfig: {
+      accessKeyId: config.winston.aws.awsConfig.accessKeyId || '',
+      secretAccessKey: config.winston.aws.awsConfig.secretAccessKey || '',
+      region: config.winston.aws.awsConfig.region || '',
+    },
+  }
 });
