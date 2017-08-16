cfe550f444e47dc77298437817b99a3232dfc95e
James Mullaney <james@ht2.co.uk>
2017-08-16 14:54:14 +0000

    Moved test

--- dist/mongoModelsRepo/utils/getVoiders.d.ts
+++
@@ -1,8 +0,0 @@
-import GetVoidersOptions from '../../repoFactory/options/GetVoidersOptions';
-import Config from '../Config';
-export interface Options extends GetVoidersOptions {
-    config: Config;
-    searchKey: string;
-}
-declare const _default: ({config, searchKey, ids}: Options) => Promise<any[]>;
-export default _default;
