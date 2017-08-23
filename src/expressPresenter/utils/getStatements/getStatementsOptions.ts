40fd4553ebae549eda5641b63584c6c5238cdfd7
James Mullaney <james@ht2.co.uk>
2017-08-23 12:15:09 +0000

    queryParam checks

--- src/expressPresenter/utils/getStatements/getStatementsOptions.ts
+++ src/expressPresenter/utils/getStatements/getStatementsOptions.ts
@@ -2,20 +2,32 @@ import GetStatementsOptions from '../../../serviceFactory/options/GetStatementsO
 import parseJson from './../../../utils/parseJson';
 import * as boolean from 'boolean';
 
+/**
+ * We pass back undefined where possible to avoid extra params being added to the
+ * more link unnecessarily
+ */
 export default (queryParams: any): Partial<GetStatementsOptions> => {
   return {
-    agent: queryParams.agent !== undefined ? parseJson(queryParams.agent, ['query', 'agent']) : undefined,
-    verb: queryParams.verb,
-    activity: queryParams.activity,
-    registration: queryParams.registration,
+    agent:
+      queryParams.agent !== undefined ? parseJson(queryParams.agent, ['query', 'agent']) : undefined,
+    verb:
+      queryParams.verb,
+    activity:
+      queryParams.activity,
+    registration:
+      queryParams.registration,
     related_activities:
       queryParams.related_activities !== undefined ? boolean(queryParams.related_activities) : undefined,
     related_agents:
       queryParams.related_agents !== undefined ? boolean(queryParams.related_agents) : undefined,
-    since: queryParams.since,
-    until: queryParams.until,
-    limit: Number(queryParams.limit),
-    ascending: boolean(queryParams.ascending),
+    since:
+      queryParams.since,
+    until:
+      queryParams.until,
+    limit:
+      queryParams.limit !== undefined ? Number(queryParams.limit) : undefined,
+    ascending:
+      queryParams.ascending !== undefined ? boolean(queryParams.ascending) : undefined,
     cursor: queryParams.cursor,
   };
 };
--- src/expressPresenter/utils/getStatements/getStatementsResultOptions.ts
+++ src/expressPresenter/utils/getStatements/getStatementsResultOptions.ts
@@ -4,6 +4,7 @@ import * as boolean from 'boolean';
 export default (queryParams: any): StatementsResultOptions => {
   return {
     format: queryParams.format,
-    attachments: boolean(queryParams.attachments)
+    attachments:
+      queryParams.attachments !== undefined ? boolean(queryParams.attachments) : undefined
   };
 };
