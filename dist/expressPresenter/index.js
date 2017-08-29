5060a0445c37b40ae7d57b6ac94de4d084c88ffd
Ryan Smith <0ryansmith1994@gmail.com>
2017-08-29 15:25:43 +0100

    Adds express route.

--- dist/expressPresenter/index.js
+++ dist/expressPresenter/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var expressPresenter_1 = require("jscommons/dist/expressPresenter");
 var getAbout_1 = require("./getAbout");
 var getDemoAuth_1 = require("./getDemoAuth");
+var getFullActivity_1 = require("./getFullActivity");
 var getStatements_1 = require("./getStatements");
 var postStatements_1 = require("./postStatements");
 var putStatement_1 = require("./putStatement");
@@ -10,6 +11,7 @@ exports.default = function (config) {
     var router = expressPresenter_1.default(config);
     router.get('/auth', getDemoAuth_1.default(config));
     router.get('/xAPI/about', getAbout_1.default(config));
+    router.get('/xAPI/activities', getFullActivity_1.default(config));
     router.get('/xAPI/statements', getStatements_1.default(config));
     router.put('/xAPI/statements', putStatement_1.default(config));
     router.post('/xAPI/statements', postStatements_1.default(config));
