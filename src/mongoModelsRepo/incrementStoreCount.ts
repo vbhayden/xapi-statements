c42e61ec24234b548861001f1b0a938acda9f020
James Mullaney <james@ht2.co.uk>
2017-09-08 09:19:36 +0100

    Fixes mongo increment on store count

--- src/mongoModelsRepo/incrementStoreCount.ts
+++ src/mongoModelsRepo/incrementStoreCount.ts
@@ -5,6 +5,6 @@ import { ObjectID } from 'mongodb';
 export default (config: Config) => {
   return async (opts: IncrementStoreCountOptions): Promise<void> => {
     const collection = (await config.db).collection('lrs');
-    await collection.updateOne({ lrs_id: new ObjectID(opts.client.lrs_id) }, { $inc: { statementCount: opts.count } });
+    await collection.updateOne({ _id: new ObjectID(opts.client.lrs_id) }, { $inc: { statementCount: opts.count } });
   };
 };
