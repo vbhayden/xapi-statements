e7d9e53bc977a8cb4371b7c7eb42271f7cdc55f5
James Mullaney <james@ht2.co.uk>
2017-09-28 15:41:54 +0100

    perf(eagerLoadUpRefs): Ensured IDs on eager loaded uprefs
    * perf(eagerLoadUpRefs): Ensured IDs on eager loaded uprefs
    
    * fix(eagerLoadUpRefs): Remove bad line
    
    * fix(eagerLoadUpRefs): Attempts to fix return type.
    
    * fix(eagerLoadUpRefs): Spreads populatedGroupedUpRefIds.
    
    * fix(eagerLoadUpRefs): Fixes lint errors.
    
    * fix(eagerRefs): Fixes types

--- src/service/storeStatements/updateReferences/eagerLoadUpRefs.ts
+++ src/service/storeStatements/updateReferences/eagerLoadUpRefs.ts
@@ -8,18 +8,29 @@ export default async (
   config: Config,
   models: UnstoredStatementModel[],
   client: ClientModel
-): Promise<Dictionary<String[]>> => {
+): Promise<Dictionary<string[]>> => {
   const statementIds = models.map((model) => {
     return model.statement.id;
   });
+
+  const emptyGroupedUpRefIds = statementIds.reduce(
+    (result, statementId) => {
+      return {
+        ...result,
+        [statementId]: [],
+      };
+    },
+    {}
+  );
+
   const eagerLoadedUpRefs = await config.repo.getUpRefsByIds({ targetIds: statementIds, client });
   const groupedUpRefs = groupBy(eagerLoadedUpRefs, (upRef: UpRef) => {
     return upRef.targetId;
   });
-  const groupedUpRefIds = mapValues(groupedUpRefs, (upRefs: UpRef[]): string[] => {
+  const populatedGroupedUpRefIds = mapValues(groupedUpRefs, (upRefs: UpRef[]): string[] => {
     return upRefs.map((upRef: UpRef) => {
       return upRef.sourceId;
     });
   });
-  return groupedUpRefIds;
+  return { ...emptyGroupedUpRefIds, ...populatedGroupedUpRefIds };
 };
