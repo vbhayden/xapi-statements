b3ab785626b90f92ef6895a9d25cd92a0cd229ba
Ryan Smith <0ryansmith1994@gmail.com>
2017-10-31 11:26:56 +0000

    ci(travis): Fixes code coverage.

--- src/tracker.ts
+++ src/tracker.ts
@@ -3,6 +3,7 @@ import config from './config';
 export type Tracker = (name: string, value: any) => void;
 
 export default async (): Promise<Tracker> => {
+  /* istanbul ignore next */
   if (config.tracker.newrelic.enabled === true) {
     const newrelic = require('newrelic');
     const tracker: Tracker = (name, value) => {
