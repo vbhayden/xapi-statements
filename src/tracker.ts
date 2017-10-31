7f6829431a83977848c0c9f625468e3e912f4092
Ryan Smith <0ryansmith1994@gmail.com>
2017-10-31 10:53:16 +0000

    fix(tracker): Replaces the dynamic import with require.

--- src/tracker.ts
+++ src/tracker.ts
@@ -4,7 +4,7 @@ export type Tracker = (name: string, value: any) => void;
 
 export default async (): Promise<Tracker> => {
   if (config.tracker.newrelic.enabled === true) {
-    const newrelic = await import('newrelic');
+    const newrelic = require('newrelic');
     const tracker: Tracker = (name, value) => {
       newrelic.addCustomParameter(name, value);
     };
